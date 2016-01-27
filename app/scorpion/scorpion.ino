81fac7277f3cd0cef382f7c5764e31400c2b9b12
ryanmattes <ryanm@horsefish.net>
2016-01-26 19:36:30 -0600

    Added install images

--- app/scorpion/scorpion.ino
+++ app/scorpion/scorpion.ino
@@ -29,7 +29,7 @@ const boolean debug = true;
 // #################### SETUP ####################
 void setup() {
   // Start serial monitor
-  if(debug)Serial.begin(19200);
+  if(debug)Serial.begin(9600);
   
   // Initializes motor. Doesn't cause it to start firing.
   analogWrite(motor,HIGH);
@@ -46,16 +46,21 @@ void setup() {
   digitalWrite(power_trigger,HIGH);
 
   // Initialize selector and tappet sensors as OFF
-  digitalWrite(power_position,LOW);
-  digitalWrite(power_selector,LOW);
+  //digitalWrite(power_position,LOW);
+  //digitalWrite(power_selector,LOW);
+  Serial.println("Initializing");
+  digitalWrite(power_position,HIGH);
+  digitalWrite(power_selector,HIGH);
+
 }
 // #################### SELECTOR MODE ####################
 boolean isauto(){
-  // semi reads between 0.27 and 0.28
-  // auto reads 0.36 
-  // adjust the >0.30 below to set the breakpoint 
+  // semi reads between 0.32 and 0.34
+  // auto reads 0.47 
+  // adjust the >0.40 below to set the breakpoint 
   // where it should mean full auto
-  return ((analogRead(io_selector) *(1.00/1023.00))>0.30);
+//  Serial.println(analogRead(io_selector) *(1.00/1023.00));
+  return ((analogRead(io_selector) *(1.00/1023.00))>0.40);
 }
 // #################### TRIGGER PULL ####################
 boolean trigger(){
@@ -65,10 +70,12 @@ boolean trigger(){
 }
 // #################### SENSOR POSITION ####################
 float position(){
+  Serial.println(analogRead(io_position));
   return analogRead(io_position) *(1.00/1023.00);
 }
 // #################### WATCH FIRE CYCLE ####################
 void watchcycle(){
+    //Serial.println(position());
     // Check if tappet has reached rear position
     if (position() <= .48)
     {
@@ -78,12 +85,14 @@ void watchcycle(){
     // and THEN check if it has hit front position
     if (rearposition && position() >= .49)
     {
+      Serial.println("Cycle: Front Position");
       frontposition = true;
     }
 
     // tappet has fully cycled, increment cycle
     if (frontposition && rearposition)
     {
+      Serial.println("Cycle Complete");
       rearposition = false;
       frontposition = false;
       cyclecount = cyclecount + 1;
@@ -91,19 +100,15 @@ void watchcycle(){
 }
 // #################### LOOP ####################
 void loop() {
- 
   // Is trigger pulled?
   if(trigger()){
     // turn on selector and tapppet sensors
     // This may need to be moved back up to setup if it acts wonky. 
     // I don't know how long the sensor takes to generate a reading, 
     // or the impact of writing it HIGH repeatedly. 
-    digitalWrite(power_position,HIGH);
-    digitalWrite(power_selector,HIGH);
 
     // determine how many times to fire
     int rounds = (isauto()?autorounds:semirounds);
-    
     // listen for cycle count
     watchcycle();
     
@@ -124,9 +129,8 @@ void loop() {
     // motor off
     digitalWrite(motor,LOW);
     // Turn off selector and tappet sensors
-    digitalWrite(power_position,LOW);
-    digitalWrite(power_selector,LOW);
-
+    //digitalWrite(power_position,LOW);
+    //digitalWrite(power_selector,LOW);
   }
 }
 
